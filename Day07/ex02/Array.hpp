47ff8e97306da219f8eac369309a076f212d52f0
Pinkyboi <ayman.benaissa2000@gmail.com>
2021-09-18 18:10:05 +0100

    start ex02

--- Day07/ex02/Array.hpp
+++ Day07/ex02/Array.hpp
@@ -6,30 +6,82 @@
 /*   By: abenaiss <abenaiss@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/17 18:12:01 by abenaiss          #+#    #+#             */
-/*   Updated: 2021/09/17 18:15:33 by abenaiss         ###   ########.fr       */
+/*   Updated: 2021/09/18 18:07:29 by abenaiss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef __ARRAY__
 #define __ARRAY__
 
+template<typename Container>
 class Array
 {
     private:
-
+        unsigned int    _arrayLenght;
     public:
-        Array(void);
-        Array(unsigned int n);
-        ~Array();
+        Container       *arrayContainer;
+    public:
+                        Array(void);
+                        Array(unsigned int arrayLenght);
+                        Array(const Array& arrayInstance);
+        unsigned int    size(void);
+        Array&          operator=(const Array& arrayInstance);
+        Container       operator[](unsigned int i);
+                        ~Array();
 };
 
-Array::Array(void)
+template<typename Container>
+Array<Container>::Array(void):_arrayLenght(arrayLenght)
+{
+    arrayContainer = new Container[];
+}
+
+template<typename Container>
+Array<Container>::Array(unsigned int arrayLenght):_arrayLenght(arrayLenght)
+{
+    arrayContainer = new Container[this->_arrayLenght];
+}
+
+template<typename Container>
+Array<Container>::Array(const Array& arrayInstance)
+{
+    *this = arrayInstance; 
+}
+
+template<typename Container>
+Array<Container>::~Array()
 {
 }
 
-Array::~Array()
+template<typename Container>
+unsigned int Array<Container>::size()
 {
+    return this->_arrayLenght;
 }
 
+template<typename Container>
+Array<Container>& Array<Container>::operator=(const Array& arrayInstance)
+{
+    if(this == &arrayInstance)
+        return *this;
+    delete[] this->arrayContainer;
+    unsigned int arrayInstanceSize = arrayContainer.size();
+    this->arrayContainer = new Container[arrayInstanceSize];
+    for(int i = 0; i < arrayInstanceSize; i++)
+        this->arrayContainer[i] = arrayInstance[i];
+}
+
+template<typename Container>
+Container Array<Container>::operator[](unsigned i)
+{
+    try
+    {
+        return this->arrayContainer[i];
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+}
 
 #endif
--- Day07/ex02/main.cpp
+++ Day07/ex02/main.cpp
@@ -3,11 +3,63 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: abenaiss <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: abenaiss <abenaiss@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/17 18:12:08 by abenaiss          #+#    #+#             */
-/*   Updated: 2021/09/17 18:12:09 by abenaiss         ###   ########.fr       */
+/*   Updated: 2021/09/18 17:12:06 by abenaiss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+#include <Array.hpp>
 
+#define MAX_VAL 750
+int main(int, char**)
+{
+    Array<int> numbers(MAX_VAL);
+    int* mirror = new int[MAX_VAL];
+    srand(time(NULL));
+    for (int i = 0; i < MAX_VAL; i++)
+    {
+        const int value = rand();
+        numbers[i] = value;
+        mirror[i] = value;
+    }
+    //SCOPE
+    {
+        Array<int> tmp = numbers;
+        Array<int> test(tmp);
+    }
+
+    for (int i = 0; i < MAX_VAL; i++)
+    {
+        if (mirror[i] != numbers[i])
+        {
+            std::cerr << "didn't save the same value!!" << std::endl;
+            return 1;
+        }
+    }
+    try
+    {
+        numbers[-2] = 0;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+    try
+    {
+        numbers[MAX_VAL] = 0;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+
+    for (int i = 0; i < MAX_VAL; i++)
+    {
+        numbers[i] = rand();
+    }
+    delete [] mirror;//
+    return 0;
+}
