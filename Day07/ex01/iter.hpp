ba7ac28a3fc2bc82adb9f0b204a809d4bb428108
Pinkyboi <ayman.benaissa2000@gmail.com>
2021-09-18 14:33:25 +0100

    fix minor bugs on the ex00 and ex01

--- Day07/ex01/iter.hpp
+++ Day07/ex01/iter.hpp
@@ -6,7 +6,7 @@
 /*   By: abenaiss <abenaiss@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/17 17:23:06 by abenaiss          #+#    #+#             */
-/*   Updated: 2021/09/17 18:04:40 by abenaiss         ###   ########.fr       */
+/*   Updated: 2021/09/18 13:16:38 by abenaiss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,12 @@
 #include <iostream>
 
 template<typename T>
-void iter(const T* adress, int lenght, std::function<void (const T& x )> func)
+void iter(const T* address, int lenght, void (*function)(const T&))
 {
+    if (!address)
+        return ;
     for(int i = 0; i < lenght; i++)
-        func(adress[i]);
+        function(address[i]);
 }
 
 #endif
--- Day07/ex01/main.cpp
+++ Day07/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: abenaiss <abenaiss@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/17 17:23:16 by abenaiss          #+#    #+#             */
-/*   Updated: 2021/09/17 18:05:46 by abenaiss         ###   ########.fr       */
+/*   Updated: 2021/09/18 13:10:46 by abenaiss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,11 +31,6 @@ std::ostream & operator<<( std::ostream & o, Awesome const & rhs ) { o << rhs.ge
 template< typename T >
 void print(const T& x) { std::cout << x << std::endl; return; }
 
-template<>
-void print<Awesome>(const Awesome& x)
-{
-    std::cout << x.get() << std::endl; return; 
-}
 
 
 int main()
@@ -43,8 +38,8 @@ int main()
     int tab[] = { 0, 1, 2, 3, 4 };
     Awesome tab2[5];
 
-    iter<int>( tab, 5, print<int>);
-    iter<Awesome>( tab2, 5, print<Awesome>);
+    iter( tab, 5, print);
+    iter( tab2, 5, print);
 
     return 0;
 }
