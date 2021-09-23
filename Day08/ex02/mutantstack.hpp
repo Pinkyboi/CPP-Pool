42a5ea01373ed40ceb7b9a19f5584e74b50f2089
Pinkyboi <ayman.benaissa2000@gmail.com>
2021-09-23 18:28:39 +0100

    minor modification on day08/ex02

--- Day08/ex02/mutantstack.hpp
+++ Day08/ex02/mutantstack.hpp
@@ -6,28 +6,29 @@
 /*   By: abenaiss <abenaiss@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/21 14:40:25 by abenaiss          #+#    #+#             */
-/*   Updated: 2021/09/22 18:47:02 by abenaiss         ###   ########.fr       */
+/*   Updated: 2021/09/23 16:00:15 by abenaiss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
-#include <deque>
 #include <stack>
 template <typename T>
 class MutantStack : public std::stack<T>
 {
+    public :
+        typedef typename std::stack<T>::container_type  container_type;
+        typedef typename container_type::iterator       iterator;
     public:
-        typedef typename std::stack<T>::container_type::iterator iterator;
-                                            MutantStack(void){};
-                                            MutantStack(const MutantStack& stackInstance){*this = stackInstance;};
-                                            ~MutantStack(){};
-        typename std::deque<T>::iterator    begin(void){return std::stack<T>::c.begin();};
-        typename std::deque<T>::iterator    end(void){return std::stack<T>::c.end();};
-        MutantStack&                        operator=(const MutantStack& stackInstance)
+                        MutantStack(void){};
+                        MutantStack(const MutantStack& stackInstance){*this = stackInstance;};
+                        ~MutantStack(){};
+        iterator        begin(void){return this->c.begin();};
+        iterator        end(void){return this->c.end();};
+        MutantStack&    operator=(const MutantStack& stackInstance)
         {
             if(this == &stackInstance)
                 return *this;
-            typename std::stack<T>::container_type temp(stackInstance.c);
+            container_type temp(stackInstance.c);
             std::swap(std::stack<T>::c, temp);
             return *this;
         };
